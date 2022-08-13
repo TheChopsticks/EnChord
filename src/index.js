b8500ce139a21c7f5d60ea6746ddf72ffdbcd260
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-08-13 14:07:37 +0400

    Create game class and the array of notes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// An array of note and octaves.
+const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+class Game {
+    constructor() {
+        this.score = 0;
+        this.level = 'Easy';
+        this.numOfQues = 10;
+    }
+
+}
