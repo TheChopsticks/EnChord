fb50ebe4fdb6148fa13d38d8fc9547aef155a20f
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-08-26 10:55:42 +0200

    Create elements for the start page and append them to the main container

--- src/view.js
+++ src/view.js
@@ -3,7 +3,19 @@ export class View {
     this.appContainer = musicApp;
   }
 
-  // renderStartPage()
+  renderStartPage() {
+    // Create elements
+    const gameTitle = document.createElement('h1');
+    const gameRuleParagraph = document.createElement('p');
+    const startButton = document.createElement('button');
+
+    // Append Elements to the main app container.
+    this.appContainer.append(gameTitle, gameRuleParagraph, startButton);
+
+    gameTitle.textContent = 'Cool name for music app';
+    gameRuleParagraph.textContent = 'Guess the interval between the 2 tones.';
+    startButton.textContent = 'Start';
+  }
 
   // renderQuestion()
 
