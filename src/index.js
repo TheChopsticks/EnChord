1584172f332250e740aa4217a3e3f2b722c2b76a
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-09-12 17:26:46 +0400

    Add a basic View class (#11)
    * Add a main container to wrap Music App
    
    * Create the View class
    
    * Import View class to index.js
    
    * Create elements for the start page and append them to the main container
    
    * Create elements for the question page
    
    * create and append a div to display the current score
    
    * Change some variable names to be more descriptive
    
    * Add a function to update current score in the score display panel
    
    * Add a function to monitor the playTonesButton's click count and change the button's textContent to 'Replay'
    
    * When nextQuestionButton is clicked, save an user answer, reset the playTonesButton's click count and load a next question.
    
    * Add a span element to update the current question number
    
    * Add a function to reset previous game data when render the start page to restart the game
    
    * Add a function to update the current question number when moving to a next question
    
    * Delete comment lines
    
    * Update some variables' name
    
    * Update some variables' name
    
    * Change function name from saveUserAnswer to checkUserAnswer
    
    * Fix typos
    
    * Add a private function to create buttons
    
    * Create interval buttons again using private method #createButton
    
    * Reorganize code by grouping by elements
    
    * Add a private function to create HTML element
    
    * Implement #createElement method
    
    * Refactor
    
    * Add a function to check if playTonesButton is clicked and change the button's textContent
    
    * Add a function to update textContent of spans for current question number and user score
    
    * Add receivedGameData parameter in updateQuestionPage function

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { View } from './view';
+
+const musicApp = document.getElementById('musicApp');
+
+// eslint-disable-next-line no-unused-vars
+const view = new View(musicApp);
---
+++ src/view.js
@@ -0,0 +1,143 @@
+export class View {
+  constructor(musicApp) {
+    this.appContainer = musicApp;
+    this.isPlayTonesButtonClicked = false;
+  }
+
+  #createButton(buttonText) {
+    return this.#createElement('button', buttonText);
+  }
+
+  #createElement(elementType, elementText = '') {
+    const element = document.createElement(elementType);
+    element.textContent = elementText;
+    return element;
+  }
+
+  renderStartPage() {
+    const gameTitle = this.#createElement('h1', 'Cool name for music app');
+    const gameRuleParagraph = this.#createElement(
+      'p',
+      'Guess the interval between the 2 tones.'
+    );
+    const gameStartButton = this.#createButton('Start');
+
+    this.appContainer.append(gameTitle, gameRuleParagraph, gameStartButton);
+    gameStartButton.addEventListener(
+      'click'
+      // gameStartButton clicked event
+    );
+  }
+
+  renderQuestionPage() {
+    const currentQuestionNumberDisplay = this.#createElement(
+      'div',
+      'Question: '
+    );
+    const currentQuestionNumberSpan = this.#createElement('span');
+    currentQuestionNumberSpan.id = 'questionNumber';
+    currentQuestionNumberDisplay.append(currentQuestionNumberSpan);
+
+    const playTonesButton = this.#createButton('Play tones');
+    const gameRuleParagraph = this.#createElement(
+      'p',
+      'Guess the interval between the 2 tones.'
+    );
+
+    const buttonsGridContainer = this.#createElement('div');
+
+    const submitAndMoveToNextQuestionButton =
+      this.#createButton('Move to next');
+    const skipQuestionButton = this.#createButton('Skip');
+
+    const currentScoreDisplay = this.#createElement('div', 'Score: ');
+    const currentScoreSpan = this.#createElement('span');
+    currentScoreSpan.id = 'currentScore';
+    currentScoreDisplay.append(currentScoreSpan);
+
+    this.appContainer.append(
+      currentQuestionNumberDisplay,
+      playTonesButton,
+      gameRuleParagraph,
+      buttonsGridContainer,
+      skipQuestionButton,
+      submitAndMoveToNextQuestionButton,
+      currentScoreDisplay
+    );
+
+    const minor2nd = this.#createButton('Minor 2nd');
+    const major2nd = this.#createButton('Major 2nd');
+    const minor3rd = this.#createButton('Minor 3rd');
+    const major3rd = this.#createButton('Major 3rd');
+    const perfect4th = this.#createButton('Perfect 4th');
+    const tritone = this.#createButton('Tritone');
+    const perfect5th = this.#createButton('Perfect 5th');
+    const minor6th = this.#createButton('Minor 6th');
+    const major6th = this.#createButton('Major 6th');
+    const minor7th = this.#createButton('Minor 7thd');
+    const major7th = this.#createButton('Major 7th');
+
+    buttonsGridContainer.append(
+      minor2nd,
+      major2nd,
+      minor3rd,
+      major3rd,
+      perfect4th,
+      tritone,
+      perfect5th,
+      minor6th,
+      major6th,
+      minor7th,
+      major7th
+    );
+
+    playTonesButton.addEventListener('click', () => {
+      // playTonesButton clicked event
+      this.isPlayTonesButtonClicked = true;
+      this.#changePlayTonesButtonText();
+    });
+
+    skipQuestionButton.addEventListener(
+      'click'
+      // answerPublished event
+    );
+
+    submitAndMoveToNextQuestionButton.addEventListener(
+      'click'
+      // answerPublished event
+    );
+  }
+
+  updateQuestionPage(
+    // receivedGameData,
+    currentQuestionNumber,
+    currentUserScore
+  ) {
+    this.isPlayTonesButtonClicked = false;
+    const currentQuestionNumberSpan = document.getElementById('questionNumber');
+    currentQuestionNumberSpan.textContent = currentQuestionNumber;
+    const currentScoreSpan = document.getElementById('currentScore');
+    currentScoreSpan.textContent = currentUserScore;
+  }
+
+  #changePlayTonesButtonText() {
+    if (this.isPlayTonesButtonClicked) {
+      this.playTonesButton.textContent = 'Replay tones';
+    }
+  }
+
+  renderResults() {
+    const finalUserScoreDisplay = this.#createElement('div');
+    const finalUserScore = this.#createElement('span');
+    finalUserScoreDisplay.append(finalUserScore);
+
+    const playGameAgainButton = this.#createButton('Play again!');
+
+    this.appContainer.append(finalUserScoreDisplay, playGameAgainButton);
+
+    playGameAgainButton.addEventListener(
+      'click'
+      // playGameAgainButton clicked event.
+    );
+  }
+}
