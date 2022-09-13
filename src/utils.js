c5a1348957e530b498a7ccd2a7e73383aa7a9dd6
Sandy S Kuo <62935115+kuosandys@users.noreply.github.com>
2022-09-13 22:06:02 +0200

    Add jest as a dev dependency for testing

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-/**
- * Converts a given string to sentence case.
- * @param {string} text
- * @returns {string}
- */
-export function toSentenceCase(text) {
-  if (!text.length) return text;
-  return text[0].toLocaleUpperCase() + text.slice(1).toLocaleLowerCase();
-}
